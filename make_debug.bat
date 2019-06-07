fa6d68c4851baa4eb52236c89d5a7e051d3a01aa
Corbin Stark <starkcorbin@gmail.com>
2019-06-06 22:20:20 -0700

    Fixed make_debug script

--- make_debug.bat
+++ make_debug.bat
@@ -3,5 +3,5 @@
 mkdir build
 pushd build
 cls
-cl /Zi /EHsc -I..\include ..\*.cpp ..\*.c ..\libs\*.lib msvcrt.lib shell32.lib user32.lib gdi32.lib opengl32.lib
+cl /Zi /EHsc -I..\include ..\main.cpp ..\*.cpp ..\ENGINE\*.c ..\ENGINE\*.cpp ..\libs\*.lib msvcrt.lib shell32.lib user32.lib gdi32.lib opengl32.lib
 popd
